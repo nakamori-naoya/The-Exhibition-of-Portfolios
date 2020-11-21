9f8285ed82daff01c211b12840279c16304858a3
nakamori-naoya <nmk623tk@gmail.com>
2020-11-21 20:11:21 +0900

    あ

--- src/templates/CardList.jsx
+++ src/templates/CardList.jsx
@@ -1,33 +1,55 @@
 import { Grid } from '@material-ui/core';
 import React, {useMemo} from 'react'; 
+import {makeStyles} from "@material-ui/styles"
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-const sliceByNumber = (array , number) => {
-  const length = Math.ceil(array.length / number)
-  //配列の要素が10で、numberが3のときは、ceilで切り上げするので、lengthは4で新たな配列の要素数が4になる。
 
-  return new Array(length).fill()
-    .map((_, i) =>array.slice(i * number, (i + 1) * number)
-     //slice(begin, end)で使い、endのindexは含まない
-  )
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+}));
 
 const CardList = ({dataSet}) => {
-
-  const dividedDataSet = useMemo(() => sliceByNumber(dataSet, 3),[]);
-   
+  const classes = useStyles();
    return (
      <>
-       
-          <Grid container direction="row" justify="center" alignItems="center">
-          {dividedDataSet.map((datas) =>(
-             datas.map((data) => (
-               <Grid item  xs={4} color="primary">
-               {data.name}
-               </Grid>
-             ))
-       ))}
+      <Grid container direction="row" justify="space-around" alignItems="center" spacing={3}>
+          {dataSet.map((datas) =>(
+          <Grid item  xs={4} >
+            <Card>
+              <CardMedia
+                className={classes.media}
+                image="../images/fightClub.png"
+              />
+              <Typography variant="body2" color="textSecondary" component="p">
+               {datas.lesson}
+               This impressive paella is a perfect party dish and a fun meal to cook together with your
+               guests. Add 1 cup of frozen peas along with the mussels, if you like.
+             </Typography>
+            </Card>  
           </Grid>
+       ))}
+      </Grid>
      </>
    )
 }
+
 export default CardList;
