cd22ebd5ae8cf1d03fe844da111c2b09540cce88
nakamori-naoya <nmk623tk@gmail.com>
2020-11-21 18:23:42 +0900

    a

---
+++ src/dataSet.js
@@ -0,0 +1,14 @@
+export const dataSet  =[
+  {name: "naoya", lesson: "React", paycheck: 5000},
+  {name: "takashi", lesson: "数学", paycheck: 2000 },
+  {name: "masaki", lesson: "英語", paycheck: 3000 },
+  {name: "Jin", lesson: "Vue", paycheck: 4000},
+  {name: "Joe", lesson: "Angular", paycheck: 4000},
+  {name: "Jack", lesson: "Rails", paycheck: 3000},
+  {name: "Maki", lesson: "PHP", paycheck: 2500},
+  {name: "Mami", lesson: "Python", paycheck: 3000},
+  {name: "yuuma", lesson: "Java", paycheck: 3000},
+  {name: "taita", lesson: "TypeScript", paycheck: 4000},
+  {name: "kei", lesson: "JavaScript", paycheck: 3000},
+  {name: "Sou", lesson: "Next.js", paycheck: 2200},
+]
