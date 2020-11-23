b89d720913dca4f759cf1fc54ca91d6554f94d0c
nakamori-naoya <nmk623tk@gmail.com>
2020-11-23 09:07:00 +0900

    ggogleログインボタンの実装

--- src/UIkit/GoogleButton.jsx
+++ src/UIkit/GoogleButton.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import Button from "@material-ui/core/Button"
 import {makeStyles} from "@material-ui/styles"
 import { Icon } from "@material-ui/core"
+import google from "../assets/img/google.png"
 
 const useStyles = makeStyles ({
   button: {
     color: "#000",
-    fontSize: 18,
+    fontSize: 15,
     height: 48,
     width: 256
   },
@@ -25,6 +26,7 @@ const GoogleButton = (props) => {
           onClick = {() => props.onClick()} 
           color="secondary"
           >
+      <img alt="" src={google} className={classes.icon} />
       {props.label} 
       </Button>
   </label>
