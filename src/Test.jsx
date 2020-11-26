cb821cd074f89088738d65137eb6e0a49bcaa439
nakamori-naoya <nmk623tk@gmail.com>
2020-11-26 11:00:17 +0900

    dd

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,24 +1,27 @@
 import React, {useState, useCallback} from 'react'
 import { SelectButton } from './UIkit/SelectButton';
 import { RaderPlot } from './components/RaderPlot';
+import LightBlueButton from './UIkit/LightBlueButton';
 
 export const Test = () => {
   const [usability, setUsability] = useState(0);
   const [businessOriented, setBusinessOriented] = useState(0);
-  //const [sociality, setSociality] = useState(0);
+  const [sociality, setSociality] = useState(0);
+  const [points, setPoints] = useState(usability, businessOriented,sociality);
 
-  const [points, setPoints] = useState([usability, businessOriented])
-
+  
 
   const  selectUsability  = useCallback((event) => {
     setUsability(event.target.value)
-    return setPoints([event.target.value, businessOriented])
   }, [setUsability]);
 
   const  selectBusinessOriented  = useCallback((event) => {
     setBusinessOriented(event.target.value)
-    return setPoints([usability, event.target.value])
   }, [setBusinessOriented]);
+
+  const  selectSociality  = useCallback((event) => {
+    setSociality(event.target.value)
+  }, [setSociality]);
 
  
 
@@ -38,8 +41,13 @@ export const Test = () => {
       datas={[1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]} description={"ビジネス性"}   />
       
       <SelectButton
-      label={"Usabilitly"} value={usability} onChange={selectUsability} 
+      label={"社会性"} value={sociality} onChange={selectSociality} 
       datas={[1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]} description={"使いやすさを教えてください"}   />
+      <LightBlueButton
+       onClick = {() => setPoints(usability, businessOriented, sociality)} 
+       label={"登録"}
+      />
+      {console.log(setPoints)}
       <RaderPlot points={points}/>
       </div>
     </div>
