b1352769bcb777f4764284005482ea0233fbe716
nakamori-naoya <nmk623tk@gmail.com>
2020-12-03 22:40:45 +0900

    DB設計

---
+++ src/database.js
@@ -0,0 +1,18 @@
+publicUserdata
+import { PortfoliosReducer } from './reducks/Portfolios/reducers';
+-dispalyName  
+-openContacts []
+-dispalyIcon
+-uid  
+
+user
+-email
+-uid
+-username
+-createdAt
+-updatedAt
+ chats(サブコレクション)
+ [{PortfolioId, messages[{message, uid}]}]
+
+Portfolio
+
