49412523b775679ee6b0d77d761fa1fccc42273a
nakamori-naoya <nmk623tk@gmail.com>
2020-12-06 00:36:41 +0900

    editボタン実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css';
 import  "./assets/reset.css";
 import "./assets/style.css"
 import Router from "./Router"
+import Header from './templates/components/Header/Header';
 
 const App = () => {
      return (
+     <>
+       <Header />
        <main>
           <Router />
        </main>
+      </> 
   );
 }
 
---
+++ src/templates/components/Header/ClosableDrawer.jsx
@@ -0,0 +1,112 @@
+import React, {useCallback, useState} from 'react'
+import Divider from "@material-ui/core/Divider"
+import Drawer from "@material-ui/core/Drawer"
+import List from "@material-ui/core/List"
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/styles';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import HistoryIcon from '@material-ui/icons/History';
+import PersonIcon from '@material-ui/icons/Person'; 
+import ExitTOAppIcon from '@material-ui/icons/ExitToApp'; 
+import {TextInput} from '../UIkit/index'; 
+import { ListItem } from '@material-ui/core';
+import { push } from 'connected-react-router';
+import { useDispatch } from 'react-redux';
+import {signOut} from "../../reducks/users/operations"
+
+const useStyles = makeStyles((theme) => ({
+   drawer: {
+      [theme.breakpoints.up("sm")]: {
+        flexShrink: 0,
+        width: 256,
+      }
+   },
+   toolbar: theme.mixins.toolbar,
+   drawPaper: {
+     width: 256
+   },
+   searchField: {
+     allignItems: "center",
+     marginLeft: 32
+
+   }
+}))
+
+const ClosableDrawer = (props) => { 
+   const classes = useStyles();
+   const dispatch = useDispatch();
+   const {container} = props;
+   const [keyword, setKeyword] = useState("")  
+
+   const inputkeyword = useCallback((event) => {
+           setKeyword(event.target.value)
+     }, [setKeyword]) 
+
+    const selectMenu = (event, path) => {
+      dispatch(push(path))
+      props.onClose(event)
+    };
+
+
+     const menus = [
+      {func: selectMenu, label: "商品登録", icon: <AddCircleIcon/>, id: "register", value: "/product/edit"},
+     ];
+
+
+
+
+  return (
+    <nav className={classes.drawer}>
+      <Drawer
+        container={container}
+        variant="temporary"
+        anchor="right"
+        open={props.open}
+        onClose={(e) => props.onClose(e)}
+        classes={{paper: classes.drawerPaper}}
+        ModelProps={{keepMounted: true}}
+      >
+        <div
+          onClose={(e) => props.onClose(e)}
+          onKeyDown={(e) => props.onClose(e)}
+        >
+
+          <div className={classes.searchField}>
+             <TextInput 
+             fullWdth={false} label={"キーワードを入力"} multiline={false}
+             onChange={inputkeyword} required={false} rows={1} value={keyword} type={"text"}
+             />
+             <IconButton>
+                <SearchIcon/>
+             </IconButton>
+          </div>
+
+          <Divider />
+          <List>
+            {menus.map(menu =>(
+              <ListItem button key={menu.id} onClick={(e) => menu.func(e, menu.value)}>
+                <ListItemIcon>
+                  {menu.icon}
+                </ListItemIcon>
+                <ListItemText primary={menu.label} />
+              </ListItem>
+            ))}
+
+            <ListItem button key="logout" onClick={() => dispatch(signOut())}>
+               <ListItemIcon>
+                  <ExitTOAppIcon />
+               </ListItemIcon>
+               <ListItemText primary={"ログアウト"} />
+            </ListItem>
+          </List>
+
+        </div>
+      </Drawer>
+    </nav>
+  )
+
+}
+export default ClosableDrawer;
